125afdeaecf4f2a13aeb5d3b867ef6a95184cb57
Gabe <gabe@policyinnovation.org>
2024-04-16 14:09:18 -0400

    tool: added shiny busy, added category filters, added 'all' geography filter. data prep: added population served, owner type, and a couple other categorical variables to test data.

--- app/tx-dw-tool-app.R
+++ app/tx-dw-tool-app.R
@@ -15,10 +15,13 @@ library(dplyr)
 library(bivariatechoropleths)
 library(ggplot2)
 library(plotly)
+library(shinybusy)
 ###########
 ### UI #### 
 ###########
 ui <- fluidPage(
+  
+  add_busy_spinner(spin = "fading-circle"),
   # detect(),
   # useWaitress(),
   # useShinyjs(),
@@ -36,6 +39,7 @@ ui <- fluidPage(
            width = 4,
            htmlOutput("PlotlyData"),
            uiOutput("SelectGeography", style = "width: 100%"), 
+           uiOutput("SelectCat",style = "width: 100%"),
            uiOutput("VarOne", style = "width: 100%"), 
            plotlyOutput("VarOneHist", width = "250px", height = "150px"),
            uiOutput("VarTwo", style = "width: 100%"), 
@@ -48,7 +52,6 @@ ui <- fluidPage(
              width = 8),
            
            position = c("right"), fluid = TRUE),
-  
 )
 
 ################
@@ -84,22 +87,29 @@ event_two <- reactive(event_data(event = "plotly_selected", source = "b", priori
 ## Observe Event
 observeEvent(ignoreInit = TRUE, 
              list(input$Bivariate, 
-                  Controller$data_select, 
+                  # this isolate prevents rerendering loop 
+                  isolate(Controller$data_select), 
                   input$VarOne, 
                   input$VarTwo, 
-                  input$Geography, event_one(), event_two()), {
+                  input$Geography, input$Catagory, event_one(), event_two()), {
                     
 ## Remove pwsids not selected in new geo_select         
 removepwsid <- Controller$data_select$pwsid
 
+if(!str_detect(paste(input$Geography, collapse = "|"),"All"))
+{
 ## selecting new geography data based on user input                
 Controller$data_select <- Controller$data  %>%
-  filter(county_served %in% input$Geography)
-
+                          filter(county_served %in% input$Geography)
+}
+else
+{
+  Controller$data_select <- Controller$data
+}
+ 
 ## TO DO: Sort out logic here
 ## minimum data handling
 ## charts reflecting each other when shrinking data 
-
 ## event one handling
 if(!is.null(event_one()))
 {
@@ -123,13 +133,28 @@ else
   event_two_min <- min(Controller$data_select %>% pull(!!input$VarTwo), na.rm = TRUE)
 }
 
-#print("test")
+## selecting based on categorical filters
+if(!str_detect(paste(input$Catagory, collapse = "|"),"All Types"))
+{
+  #print(input$Catagory)
+  Data <- Controller$data_select %>%
+    filter(!!as.symbol(input$VarOne) <= event_one_max)%>%
+    filter(!!as.symbol(input$VarOne) >= event_one_min)%>%
+    filter(!!as.symbol(input$VarTwo) <= event_two_max)%>%
+    filter(!!as.symbol(input$VarTwo) >= event_two_min)%>%
+    filter(if_any(where(is.character), ~str_detect(., paste(input$Catagory, collapse = "|"))))
+}
+else
+{
 Data <- Controller$data_select %>%
   filter(!!as.symbol(input$VarOne) <= event_one_max)%>%
   filter(!!as.symbol(input$VarOne) >= event_one_min)%>%
   filter(!!as.symbol(input$VarTwo) <= event_two_max)%>%
-  filter(!!as.symbol(input$VarTwo) >= event_two_min) %>%
-  filter(county_served %in% input$Geography)
+  filter(!!as.symbol(input$VarTwo) >= event_two_min) 
+}
+
+# sets the data back to Controller$data_selected for UI outputs and prevents rerendering within the massive observeEvent... 
+isolate(Controller$data_select <- Data)
 
 ## If Bivariate is chosen - map color scale is different
 ## TO DO: can likely make this a lot shorter with passing the first leaflet changes to a var
@@ -179,9 +204,6 @@ else
     addLegend(pal = pal, values = colvar,  position = "bottomleft")
 }
 })
-
-
-
 ################
 #### Map #######
 ################
@@ -203,14 +225,27 @@ output$Map <- renderLeaflet({
 ## Add regions (.rmd)
 ## Add missing counties (.rmd)
 output$SelectGeography <- renderUI({
-selectizeInput("Geography","Select a Geography", choices = Counties, selected = "ANGELINA" , multiple = TRUE)
+selectizeInput("Geography","Select a Geography", choices = c("All",Counties), selected = Counties[2:10] , multiple = TRUE)
 })
 
-## Variable Select
+output$SelectCat <- renderUI({
+  ## Theres probably a way to do this with a function but curently need to copy and paste catagorical filter columns
+  Choices <- list()
+  Choices$all <- c("All Types","")
+  Choices$owner_type_description <- unique(Controller$data$owner_type_description)
+  Choices$primary_source_code <- unique(Controller$data$primary_source_code)
+  Choices$pop_catagories <- unique(Controller$data$pop_catagories)
+  Choices$tier <- unique(Controller$data$tier)
+
+  selectizeInput("Catagory", "Select a Utility Type", choices = Choices, selected = Choices$all[1], multiple = TRUE)
+})
+
+## Variable One Select
 output$VarOne <- renderUI({
-selectInput("VarOne", "Select a variable", choices = Controller$data %>% select(area_miles:total_violations_5yr) %>% colnames())
+selectInput("VarOne", "Select a variable", choices = Controller$data %>% select(estimate_mhi:total_violations_5yr) %>% colnames())
 })
 
+# Variable One Hist
 output$VarOneHist <- renderPlotly({
   req(Controller$data_select)
   req(input$VarOne)
@@ -222,6 +257,7 @@ plot_ly(x =  Controller$data_select %>% pull(!!input$VarOne), type = "histogram"
   layout( dragmode = "select")
 })
 
+## Variable Two Select
 output$VarTwo <- renderUI({
   tagList(
   selectInput("VarTwo", "Select a second variable", choices = Controller$data %>% select(pop_density:total_violations_5yr) %>% colnames() ),
@@ -229,6 +265,7 @@ output$VarTwo <- renderUI({
   )
 })
 
+# Variable Two Hist
 output$VarTwoHist <- renderPlotly({
   req(Controller$data_select)
   req(input$VarTwo)
