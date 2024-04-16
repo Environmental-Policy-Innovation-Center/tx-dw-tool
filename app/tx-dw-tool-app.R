d060c0a254a320555387c1a6060af16d35aa9a62
Gabe <gabe@policyinnovation.org>
2024-04-16 10:22:53 -0400

    base functionality for histogram selection

--- app/tx-dw-tool-app.R
+++ app/tx-dw-tool-app.R
@@ -13,6 +13,8 @@ library(leaflet.extras)
 library(htmltools)
 library(dplyr)
 library(bivariatechoropleths)
+library(ggplot2)
+library(plotly)
 ###########
 ### UI #### 
 ###########
@@ -30,10 +32,15 @@ ui <- fluidPage(
   sidebarLayout(
     div( id ="sidebar",
          sidebarPanel(
-           style = "position: fixed; height: 82%; overflow-y: auto; margin-left: -30px;", div(style = "display:inline-block; float:right; margin-bottom: 20px"),
+           style = "position: fixed; height: 82%; width: 100%; overflow-y: auto; margin-left: -30px;", div(style = "display:inline-block; float:right; margin-bottom: 20px"),
            width = 4,
+           htmlOutput("PlotlyData"),
            uiOutput("SelectGeography", style = "width: 100%"), 
            uiOutput("VarOne", style = "width: 100%"), 
+           plotlyOutput("VarOneHist", width = "250px", height = "150px"),
+           uiOutput("VarTwo", style = "width: 100%"), 
+           plotlyOutput("VarTwoHist", width = "250px", height = "150px"),
+           
          )),
            mainPanel(
              style = "margin-left: -15px;",
@@ -65,60 +72,114 @@ Controller$data <- tx_raw
 
 Counties <- unique(tx_raw$county_served)
 
-Controller$data_select <- isolate(Controller$data)
-
+event_one <- reactive(event_data(event = "plotly_selected", source = "a", priority = "event"))
+event_two <- reactive(event_data(event = "plotly_selected", source = "b", priority = "event"))
 ################
 ### Observes ###
 ################
 
 ## Responds to changes in 'Geography' selection
 ## TO DO: Fix Flashing (ONLY remove pwsids that are present currently, but not in new Controller$data_select)
-observeEvent(input$Geography,{
-  
+
+## Observe Event
+observeEvent(ignoreInit = TRUE, 
+             list(input$Bivariate, 
+                  Controller$data_select, 
+                  input$VarOne, 
+                  input$VarTwo, 
+                  input$Geography, event_one(), event_two()), {
+                    
+## Remove pwsids not selected in new geo_select         
 removepwsid <- Controller$data_select$pwsid
 
+## selecting new geography data based on user input                
 Controller$data_select <- Controller$data  %>%
-                          filter(county_served %in% input$Geography)
+  filter(county_served %in% input$Geography)
 
-colvar <-  Controller$data_select %>% pull(!!input$VarOne)
+## TO DO: Sort out logic here
+## minimum data handling
+## charts reflecting each other when shrinking data 
 
-pal <- colorNumeric(
-  palette = "Blues",
-  domain = colvar)
+## event one handling
+if(!is.null(event_one()))
+{
+  event_one_max <- max(event_one()$x, na.rm = TRUE)
+  event_one_min <- min(event_one()$x, na.rm = TRUE)
+}
+else
+{
+  event_one_max <- max(Controller$data_select %>% pull(!!input$VarOne), na.rm = TRUE)
+  event_one_min <- min(Controller$data_select %>% pull(!!input$VarOne), na.rm = TRUE)
+}
+## event two handling
+if(!is.null(event_two()))
+{
+  event_two_max <- max(event_two()$x, na.rm = TRUE)
+  event_two_min <- min(event_two()$x, na.rm = TRUE)
+}
+else
+{
+  event_two_max <- max(Controller$data_select %>% pull(!!input$VarTwo), na.rm = TRUE)
+  event_two_min <- min(Controller$data_select %>% pull(!!input$VarTwo), na.rm = TRUE)
+}
 
-leafletProxy("Map")%>%
-  removeShape(layerId = removepwsid)%>%
-  addPolygons(data = Controller$data_select, 
-              layerId = ~pwsid,
-              label = ~htmlEscape(pwsid),
-              color = ~pal(colvar),
-              weight = 1.5,
-              fill = "grey",
-              opacity = .5)
+#print("test")
+Data <- Controller$data_select %>%
+  filter(!!as.symbol(input$VarOne) <= event_one_max)%>%
+  filter(!!as.symbol(input$VarOne) >= event_one_min)%>%
+  filter(!!as.symbol(input$VarTwo) <= event_two_max)%>%
+  filter(!!as.symbol(input$VarTwo) >= event_two_min) %>%
+  filter(county_served %in% input$Geography)
 
-}, ignoreInit = TRUE, ignoreNULL = TRUE)
-
-## Responds to changes in Variable selection 
-observeEvent(input$VarOne, {
+## If Bivariate is chosen - map color scale is different
+## TO DO: can likely make this a lot shorter with passing the first leaflet changes to a var
+if(input$Bivariate == TRUE)
+{
+  leafletProxy("Map")%>%
+    clearShapes()%>%
+   # removeShape(layerId = removepwsid)%>%
+    clearControls()%>%
+    addPolygons(data = Data,
+                layerId = ~pwsid,
+                label = ~htmlEscape(pwsid),
+                weight = 1.5,
+                fill = "grey",
+                opacity = .5)%>%
+    bivariatechoropleths::addBivariateChoropleth(
+      map_data = Data,
+      var1_name = input$VarOne,
+      var2_name = input$VarTwo,
+      ntiles= 3,
+      var1_label = input$VarOne,
+      var2_label = input$VarTwo,
+      weight = 1,
+      fillOpacity = 0.7,
+      color = "grey")
+}
+else
+{
+  # subsetting to selected variable 
+  colvar <-  Data %>% pull(!!input$VarOne)
   
-  colvar <-  Controller$data_select %>% pull(!!input$VarOne)
-  pal <- colorNumeric(
+  pal <- colorQuantile(
     palette = "Blues",
-    domain = colvar)
-
+    domain = unique(colvar))
   
   leafletProxy("Map")%>%
-  clearShapes()%>%
-  addPolygons(data = Controller$data_select,
-              layerId = ~pwsid,
-              label = ~htmlEscape(pwsid),
-              color = ~pal(colvar),
-              weight = 1.5,
-              fill = "grey",
-              opacity = .5)
+    clearShapes()%>%
+   # removeShape(layerId = removepwsid)%>%
+    clearControls()%>%
+    addPolygons(data = Data,
+                layerId = ~pwsid,
+                label = ~htmlEscape(pwsid),
+                color = ~pal(colvar),
+                weight = 1.5,
+                fill = "grey",
+                opacity = .75)%>%
+    addLegend(pal = pal, values = colvar,  position = "bottomleft")
+}
+})
 
-  
-}, ignoreInit = TRUE, ignoreNULL = TRUE)
 
 
 ################
@@ -149,6 +210,37 @@ selectizeInput("Geography","Select a Geography", choices = Counties, selected = 
 output$VarOne <- renderUI({
 selectInput("VarOne", "Select a variable", choices = Controller$data %>% select(area_miles:total_violations_5yr) %>% colnames())
 })
+
+output$VarOneHist <- renderPlotly({
+  req(Controller$data_select)
+  req(input$VarOne)
+  
+plot_ly(x =  Controller$data_select %>% pull(!!input$VarOne), type = "histogram", source = "a", nbinsx = 30)%>% 
+  config(displayModeBar = FALSE) %>%
+  event_register("plotly_selected")%>%
+ # add_trace(x = density$x, y = density$y, type = "scatter", mode = "lines", fill = "tozeroy", yaxis = "y2", name = "Density") %>% 
+  layout( dragmode = "select")
+})
+
+output$VarTwo <- renderUI({
+  tagList(
+  selectInput("VarTwo", "Select a second variable", choices = Controller$data %>% select(pop_density:total_violations_5yr) %>% colnames() ),
+  checkboxInput("Bivariate", "Bivariate",value = FALSE)
+  )
+})
+
+output$VarTwoHist <- renderPlotly({
+  req(Controller$data_select)
+  req(input$VarTwo)
+  
+  plot_ly(x =  Controller$data_select %>% pull(!!input$VarTwo), type = "histogram", source = "b", nbinsx = 30)%>% 
+    config(displayModeBar = FALSE) %>%
+    event_register("plotly_selected")%>%
+    # add_trace(x = density$x, y = density$y, type = "scatter", mode = "lines", fill = "tozeroy", yaxis = "y2", name = "Density") %>% 
+    layout(dragmode = "select")
+})
+
+
 
 ################
 #### Table #####
